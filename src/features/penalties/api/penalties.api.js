d6d724aa816a36be9a2bb2cef420ac3e46b4d2de
yaxyobekuz <pubgn9642@gmail.com>
2026-04-30 09:22:59 +0500

    feat: add new API methods for penalties management including getGivenPenalties and reduce

--- src/features/penalties/api/penalties.api.js
+++ src/features/penalties/api/penalties.api.js
@@ -4,4 +4,11 @@ export const penaltiesAPI = {
   getSettings: () => http.get("/penalties/settings"),
   getById: (id) => http.get(`/penalties/${id}`),
   getMyPenalties: (params) => http.get("/penalties/my", { params }),
+  getCategories: (params) => http.get("/penalties/categories", { params }),
+  create: (data) =>
+    http.post("/penalties", data, {
+      headers: { "Content-Type": "multipart/form-data" },
+    }),
+  getGivenPenalties: (params) => http.get("/penalties/given", { params }),
+  reduce: (data) => http.post("/penalties/reduce", data),
 };
